b4e4352bc006c0422a4dbc9558a97268f2ec4950
martingutierrez23 <yeyesill2303@gmail.com>
2022-10-13 23:33:43 -0300

    proyecto final

--- tienda.js
+++ tienda.js
@@ -1,3 +1,155 @@
+// inyectando las cards
+let cards=document.getElementById("cards");
+cards.innerHTML=`<section  class="store">
+<div class="container">
+    <div class="items">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="item shadow mb-4">
+                    <h5 class="item-title">Cuaderno rojo Exito 48 hojas</h5>
+                <img class="item-image" src="./img/foto-cuaderno-rojo.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$750</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Cuaderno Exito azul 48 hojas</h5>
+                <img class="item-image" src="./img/foto-cuaderno-azul.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$750</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Cuaderno exito naranja 48 hojas</h5>
+                <img class="item-image" src="./img/foto-cuaderno-naranja.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$750</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Cuaderno Rivadavia violeta ABC </h5>
+                <img class="item-image" src="./img/cuaderno-rivadavia-abc.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$950</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Cuaderno Rivadavia celeste ABC</h5>
+                <img class="item-image" src="./img/cuaderno-rivadavia-abc-celeste.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$950</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Cuaderno Rivadavia rojo ABC</h5>
+                <img class="item-image " src="./img/cuaderno-rivadavia-abc-rojo.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$950</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapiz grafito Goldfaber HB</h5>
+                <img class="item-image" src="./img/lapiz-golfaber.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$230</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapiz grafito Trabi</h5>
+
+                <img class="item-image" src="./img/lapiz trabi.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$170</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapiz Grafito Bic</h5>
+
+                <img class="item-image" src="./img/lapiz bic.jpg">
+
+                <div class="item-details">
+                    <h4 class="item-price">$100</h4>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapicera bic</h5>
+
+                <img class="item-image" src="./img/lapicera bic.jpg">
+
+                <div class="item-details">
+                    <h5 class="item-price">$120</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapicera Pelikan</h5>
+
+                <img class="item-image" src="./img/lapicera pelikan.png">
+                <div class="item-details">
+                    <h5 class="item-price">$100</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="item shadow mb-4">
+                <h5 class="item-title">Lapicera Faber Castell</h5>
+
+                <img class="item-image" src="./img/lapicera faber castell.png">
+                <div class="item-details">
+                    <h5 class="item-price">$130</h5>
+                    <button class="item-button btn btn-primary addToCart">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</section>`;
+
+// agregando evento al boton de compras
 const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
 addToShoppingCartButtons.forEach((addToCartButton) => {
   addToCartButton.addEventListener('click', addToCartClicked);
@@ -20,7 +172,7 @@ function addToCartClicked(event) {
 
   addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
-
+// agregando las cards al carrito
 function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
   const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
     'shoppingCartItemTitle'
@@ -45,7 +197,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
     showConfirmButton: false,
     timer: 1500
   })
-
+// creando el carrito de compras
   const shoppingCartRow = document.createElement('div');
   const shoppingCartContent = `
   <div class="row shoppingCartItem">
@@ -82,7 +234,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
 
   updateShoppingCartTotal();
 }
-
+//creando la funcion para actualizar el precio
 function updateShoppingCartTotal() {
   let total = 0;
   const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
@@ -106,19 +258,19 @@ function updateShoppingCartTotal() {
   });
   shoppingCartTotal.innerHTML = `${total.toFixed(2)}$`;
 }
-
+//funcion de eliminar productos
 function removeShoppingCartItem(event) {
   const buttonClicked = event.target;
   buttonClicked.closest('.shoppingCartItem').remove();
   updateShoppingCartTotal();
 }
-
+//funcion para cambiar la cantidad de productos
 function quantityChanged(event) {
   const input = event.target;
   input.value <= 0 ? (input.value = 1) : null;
   updateShoppingCartTotal();
 }
-
+// funcion para borrar el carrito despues de realizar la compra
 function comprarButtonClicked() {
   shoppingCartItemsContainer.innerHTML = '';
   updateShoppingCartTotal();
